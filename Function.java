e3c1bef55ab8c2a5575ddea19e0f660f5427a35c
hbcbs110 <37173876+hbcbs110@users.noreply.github.com>
2022-07-20 09:32:38 -0400

    update codes

--- Function.java
+++ Function.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.lang.*;
 
 public class Function {
+    private static double pi;
+    private static double pi2;
+
+    public Function(){
+        
+    }
+
+    public static void main(String args[]){
+
+    }
+
+    public double sin(double x){
+        return 0;
+    }
+
+    public double cos(double x){
+        return 0;
+    }
+
+    public double tan(double x){
+        return 0;
+    }
 }
