78803e083aa9775604d8b0deeafe02a60865ad6b
hbcbs110 <37173876+hbcbs110@users.noreply.github.com>
2022-07-21 15:56:55 -0400

    update sin and cos

--- Function.java
+++ Function.java
@@ -2,23 +2,40 @@ import java.util.*;
 import java.lang.*;
 
 public class Function {
-    private static double pi;
-    private static double pi2;
+    private static double pi=3.14159265358979323846;
+    private static double pi2=pi*2.0;
+    private static int upper=100;
 
     public Function(){
         
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
     }
 
     public double sin(double x){
-        return 0;
+        x-=(long)(x/pi2)*pi2;
+        double m=1,f=1,a=x,res=0;
+        for(int i=0;i<=upper;i++){
+            res+=m*a/f;
+            a*=x*x;
+            m=-m;
+            f*=(2*i+2)*(2*i+3);
+        }
+        return res;
     }
 
     public double cos(double x){
-        return 0;
+        x-=(long)(x/pi2)*pi2;
+        double m=1,f=1,a=1,res=0;
+        for(int i=0;i<=upper;i++){
+            res+=m*a/f;
+            a*=x*x;
+            m=-m;
+            f*=(2*i+1)*(2*i+2);
+        }
+        return res;
     }
 
     public double tan(double x){
