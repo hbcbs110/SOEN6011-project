4cdd748f26e54d251a7a46a9af8dc6febc91fd6a
hbcbs110 <37173876+hbcbs110@users.noreply.github.com>
2022-08-05 12:40:44 -0400

    Modified

--- FunctionTest.java
+++ FunctionTest.java
@@ -37,14 +37,27 @@ public class FunctionTest {
     @org.junit.Test
     public void tan() {
         assertEquals(0.0, f.tan(0.0),1e-12);
+        System.out.println("Output of tan(0.0) is " + f.tan(0.0) + ", expecting " + 0.0);
         assertEquals(1.5574077246549022305069748074584, f.tan(1.0), 1e-12);
+        System.out.println("Output of tan(1.0) is " + f.tan(1.0) + ", expecting 1.5574077246549022305069748074584");
         assertEquals(-1.5574077246549022305069748074584, f.tan(-1.0),1e-12);
+        System.out.println("Output of tan(-1.0) is " + f.tan(-1.0) + ", expecting -1.5574077246549022305069748074584");
         assertEquals(0.54630248984379051325517946578029, f.tan(0.5), 1e-12);
+        System.out.println("Output of tan(0.5) is " + f.tan(0.5) + ", expecting 0.54630248984379051325517946578029");
         assertEquals(14.101419947171719387646083651988, f.tan(1.5), 1e-12);
+        System.out.println("Output of tan(1.5) is " + f.tan(1.5) + ", expecting 14.101419947171719387646083651988");
+        assertEquals(1255.7655915006916046605430077739, f.tan(1.57), 1e-8);
+        System.out.println("Output of tan(1.57) is " + f.tan(1.57) + ", expecting 1255.7655915006916046605430077739");
+        assertEquals(-14.101419947171719387646083651988, f.tan(-1.5), 1e-12);
+        System.out.println("Output of tan(-1.5) is " + f.tan(-1.5) + ", expecting -14.101419947171719387646083651988");
         assertEquals(-0.58721391515692907667780963564459, f.tan(1e2), 1e-12);
+        System.out.println("Output of tan(1e2) is " + f.tan(1e2) + ", expecting -0.58721391515692907667780963564459");
         assertEquals(0.0, f.tan(3.1415926535897932384626433832795), 1e-12);
+        System.out.println("Output of tan(pi) is " + f.tan(3.1415926535897932384626433832795) + ", expecting 0.0");
         assertEquals(-1.0180399498575736212457813731213, f.tan(2.5e4), 1e-10);
+        System.out.println("Output of tan(2.5e4) is " + f.tan(2.5e4) + ", expecting -1.0180399498575736212457813731213");
         assertEquals(-0.37362445398759902917349708857538, f.tan(1e6), 1e-10);
+        System.out.println("Output of tan(1e6) is " + f.tan(1e6) + ", expecting -0.37362445398759902917349708857538");
         System.out.println("Test of tan passed");
     }
 }
